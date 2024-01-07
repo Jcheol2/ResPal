4943b301fbe88ba45113788b6e8708832a314d9d
Jcheol <neng_e@naver.com>
2024-01-07 17:37:40 +0900

    feat: MyResume Fragment 작업
    - Glide로 프로필 사진 업데이트

--- app/src/main/java/com/hocheol/respal/view/MyResumeFragment.kt
+++ app/src/main/java/com/hocheol/respal/view/MyResumeFragment.kt
@@ -1,20 +1,33 @@
 package com.hocheol.respal.view
 
 import androidx.fragment.app.viewModels
+import com.bumptech.glide.Glide
 import com.hocheol.respal.R
 import com.hocheol.respal.base.BaseFragment
+import com.hocheol.respal.data.local.SharedPreferenceStorage
 import com.hocheol.respal.databinding.FragmentMyResumeBinding
+import com.hocheol.respal.repository.MainRepository
 import com.hocheol.respal.viewmodel.MainViewModel
 import com.hocheol.respal.viewmodel.MyResumeViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
-class MyResumeFragment : BaseFragment<FragmentMyResumeBinding>(R.layout.fragment_my_resume) {
+class MyResumeFragment @Inject constructor(
+    private val sharedPreferenceStorage: SharedPreferenceStorage
+) : BaseFragment<FragmentMyResumeBinding>(R.layout.fragment_my_resume) {
     private var TAG = this.javaClass.simpleName
 
     private val mainViewModel by viewModels<MainViewModel>()
     private val viewModel by viewModels<MyResumeViewModel>()
 
     override fun init() {
+        val userInfo = sharedPreferenceStorage.getUserInfo()
+        if (userInfo != null) {
+            Glide.with(this)
+                .load(userInfo.picture)
+                .into(binding.profileIconImage)
+            binding.profileUserNameText.text = userInfo.nickname
+        }
     }
 }
