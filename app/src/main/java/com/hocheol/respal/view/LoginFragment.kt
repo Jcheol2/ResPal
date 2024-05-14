d8e99ae684ae742d30a3f3efe076516cafe5f052
jcpark <neng_e@naver.com>
2024-05-14 15:59:33 +0900

    feat: LoginFragment 레이아웃 적용

--- app/src/main/java/com/hocheol/respal/view/LoginFragment.kt
+++ app/src/main/java/com/hocheol/respal/view/LoginFragment.kt
@@ -8,7 +8,9 @@ import com.hocheol.respal.base.BaseFragment
 import com.hocheol.respal.databinding.FragmentLoginBinding
 import com.hocheol.respal.viewmodel.LoginViewModel
 import com.hocheol.respal.viewmodel.MainViewModel
+import com.hocheol.respal.widget.utils.Contants.FIND_ACCOUNT_FRAGMENT_TAG
 import com.hocheol.respal.widget.utils.Contants.MY_RESUME_FRAGMENT_TAG
+import com.hocheol.respal.widget.utils.Contants.SIGN_UP_FRAGMENT_TAG
 import com.hocheol.respal.widget.utils.setOnSingleClickListener
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -20,12 +22,23 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>(R.layout.fragment_login
     private val viewModel by viewModels<LoginViewModel>()
 
     override fun init() {
+        binding.idInputText
+        binding.pwInputText
+        binding.findAccountBtn.setOnSingleClickListener {
+            mainViewModel.replaceFragment(FindAccountFragment(), null, FIND_ACCOUNT_FRAGMENT_TAG)
+        }
+        binding.loginBtn.setOnSingleClickListener {
+//            if () 로그인 성공 시 replace 하도록 변경
+            mainViewModel.replaceFragment(MyResumeFragment(), null, MY_RESUME_FRAGMENT_TAG)
+        }
+        binding.signUpBtn.setOnSingleClickListener {
+            mainViewModel.replaceFragment(SignUpFragment(), null, SIGN_UP_FRAGMENT_TAG)
+        }
         binding.githubLoginBtn.setOnSingleClickListener {
             viewModel.signInOauth(requireContext(), "github")
         }
         binding.googleLoginBtn.setOnSingleClickListener {
-//            viewModel.signInOauth(requireContext(), "google")
-            mainViewModel.replaceFragment(MyResumeFragment(), null, MY_RESUME_FRAGMENT_TAG)
+            viewModel.signInOauth(requireContext(), "google")
         }
         binding.kakaoLoginBtn.setOnSingleClickListener {
             viewModel.signInOauth(requireContext(), "kakao")
