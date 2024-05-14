3ce084f7bc9db1888def84b7da4a0f04e70e5953
jcpark <neng_e@naver.com>
2024-05-14 16:57:25 +0900

    feat: FindAccountFragment 로직 추가
    emailInputText가 수정될때 계정찾기 실패 텍스트 뷰 숨김

--- app/src/main/java/com/hocheol/respal/view/FindAccountFragment.kt
+++ app/src/main/java/com/hocheol/respal/view/FindAccountFragment.kt
@@ -1,5 +1,7 @@
 package com.hocheol.respal.view
 
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
@@ -23,11 +25,25 @@ class FindAccountFragment : BaseFragment<FragmentFindAccountBinding>(R.layout.fr
     override fun init() {
         binding.findAccountBtn.setOnSingleClickListener {
 //            if () 계정 찾기 실패 시 visible 하도록 변경
+            val emailAddress = binding.emailInputText.text.toString()
             binding.findAccountFailedView.visibility = View.VISIBLE
         }
         binding.cancelBtn.setOnSingleClickListener {
             mainViewModel.replaceFragment(LoginFragment(), null, LOGIN_FRAGMENT_TAG)
         }
-        binding.emailInputText
+        val textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                return
+            }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (binding.findAccountFailedView.visibility == View.VISIBLE) {
+                    binding.findAccountFailedView.visibility = View.GONE
+                }
+            }
+            override fun afterTextChanged(s: Editable?) {
+                return
+            }
+        }
+        binding.emailInputText.addTextChangedListener(textWatcher)
     }
 }
