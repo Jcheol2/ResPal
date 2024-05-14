8ed2917ce5776566be94243dc2d5843f270f136a
jcpark <neng_e@naver.com>
2024-05-14 15:59:52 +0900

    feat: SignUpFragment 레이아웃 적용

--- app/src/main/java/com/hocheol/respal/view/SignUpFragment.kt
+++ app/src/main/java/com/hocheol/respal/view/SignUpFragment.kt
@@ -5,17 +5,36 @@ import androidx.fragment.app.viewModels
 import com.hocheol.respal.R
 import com.hocheol.respal.base.BaseFragment
 import com.hocheol.respal.databinding.FragmentLoginBinding
+import com.hocheol.respal.databinding.FragmentSignUpBinding
 import com.hocheol.respal.viewmodel.MainViewModel
 import com.hocheol.respal.viewmodel.SignUpViewModel
+import com.hocheol.respal.widget.utils.Contants.LOGIN_FRAGMENT_TAG
+import com.hocheol.respal.widget.utils.setOnSingleClickListener
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class SignUpFragment : BaseFragment<FragmentLoginBinding>(R.layout.fragment_sign_up) {
+class SignUpFragment : BaseFragment<FragmentSignUpBinding>(R.layout.fragment_sign_up) {
     private var TAG = this.javaClass.simpleName
 
     private val mainViewModel by activityViewModels<MainViewModel>()
     private val viewModel by viewModels<SignUpViewModel>()
 
     override fun init() {
+        binding.githubLoginBtn
+        binding.googleLoginBtn
+        binding.kakaoLoginBtn
+        binding.idInputText
+        binding.pwInputText
+        binding.emailInputText
+        binding.nickNameInputText
+        binding.termsOfServiceBtn.setOnSingleClickListener {
+        }
+        binding.privacyPolicyBtn.setOnSingleClickListener {
+        }
+        binding.signUpBtn.setOnSingleClickListener {
+        }
+        binding.loginBtn.setOnSingleClickListener {
+            mainViewModel.replaceFragment(LoginFragment(), null, LOGIN_FRAGMENT_TAG)
+        }
     }
 }
