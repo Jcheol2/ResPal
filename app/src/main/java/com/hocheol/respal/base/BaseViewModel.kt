d20d6b4371081bd2a14e9c900a7ede4b93763a26
jcpark <neng_e@naver.com>
2024-05-14 16:50:53 +0900

    feat: BaseViewModel에서 코루틴 에러 발생 시 로그 기록

--- app/src/main/java/com/hocheol/respal/base/BaseViewModel.kt
+++ app/src/main/java/com/hocheol/respal/base/BaseViewModel.kt
@@ -1,23 +1,13 @@
 package com.hocheol.respal.base
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.*
 
 abstract class BaseViewModel : ViewModel() {
-    // LiveData, StateFlow 등을 여기에 정의
-
-    // CoroutineExceptionHandler를 사용하여 에러 처리
     private val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
-        // 에러 처리 로직
+        Log.e("**COROUTINE_EXCEPTION**", throwable.stackTrace.toString())
     }
 
-    // 비동기 작업을 처리하는 CoroutineScope
     protected val coroutineScope: CoroutineScope = CoroutineScope(Dispatchers.IO) + exceptionHandler
-
-    // 공통 함수 정의
-    fun performCommonAction() {
-        coroutineScope.launch {
-            // 공통 동작 수행
-        }
-    }
 }
